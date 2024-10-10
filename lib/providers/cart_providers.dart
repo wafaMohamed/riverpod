df545e50a1e78600ff5351688379356a6981629a
wafaMohamed <wafamohameddd@gmail.com>
2024-10-10 14:50:16 +0300

    generated notifier

--- lib/providers/cart_providers.dart
+++ lib/providers/cart_providers.dart
@@ -1,21 +1,25 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/models/product.dart';
+part 'cart_providers.g.dart';
 // we use a (( notifier )) with (( post )) request cause we make an event like loading etc.
 //------------------------------------------------------------------------------
 // Notifier class notify consumer widgets about state changes in the future
 // notifier is a generic class => pass data that will be working on it (type of data our state will be)
-class CartNotifier extends Notifier<Set<Product>> {
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  // instead of extends Notifier<Set<Product>> {
   // initial value in the state
   @override
   Set<Product> build() {
     // return the Notifier<Set<Product>> -> set of product as initial value
     return {
-      const Product(
-        id: '4',
-        title: 'Red Backpack',
-        price: 14,
-        image: 'assets/products/backpack.png',
-      ),
+      // remove the instance of Product make it empty
+      // const Product(
+      //   id: '4',
+      //   title: 'Red Backpack',
+      //   price: 14,
+      //   image: 'assets/products/backpack.png',
+      // ),
     };
   }
 
@@ -25,6 +29,7 @@ class CartNotifier extends Notifier<Set<Product>> {
       state = {...state, product};
     }
   }
+
   void removeProduct(Product product) {
     if (state.contains(product)) {
       state = {...state}..remove(product);
@@ -36,10 +41,22 @@ class CartNotifier extends Notifier<Set<Product>> {
 // cause the CartNotifier ((not a provider))
 // NotifierProvider<your notifier class , data type of generic in notifier>
 // return instance of your Notifier
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(
-  () {
+//-------------------------------------------------------------------------code--------------------------
+/**final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(
+    () {
     //return instance of your Notifier
     return CartNotifier();
-  },
-);
+    },
+    );
+ **/
 
+//------------------------------------------------------------------------------------------------------
+// to use generated notifier
+// 1- remove the provider -> NotifierProvider that we create
+// 2- add @riverpod annotation -> @riverpod above the class
+// 3- remove the NotifierProvider that we inherit from it
+// 4- instead of NotifierProvider -> add _$SameNameOfClass that we generated
+// 5- remove the value from initial value and return it empty
+// 6- add part file name .g.dart
+// 7- add this command:
+//
---
+++ lib/providers/cart_providers.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cart_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$cartNotifierHash() => r'868cc011e6b020d1e56c8d0afecb54429d549df9';
+
+/// See also [CartNotifier].
+@ProviderFor(CartNotifier)
+final cartNotifierProvider =
+    AutoDisposeNotifierProvider<CartNotifier, Set<Product>>.internal(
+  CartNotifier.new,
+  name: r'cartNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$cartNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CartNotifier = AutoDisposeNotifier<Set<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
